3e374d3acd613190a2cd1403d8f437b1c69d1632
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-22 19:21:21 +0530

    Add another example to showcase the advanced API in action

---
+++ example/advanced.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+from defcmd.spec import Spec
+from defcmd.runner import cmd
+
+@cmd
+def deploy(
+    host: Annotated[str, Spec(help="target hostname")]
+    ):
+    print(f"Deploying to {host}...")
+
+if __name__ == "__main__":
+    deploy.run()
