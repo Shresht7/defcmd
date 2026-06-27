0f93aa0510e67a766252cfd93bc43d099f3d58aa
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-27 22:09:56 +0530

    Add `ConfirmWidget` for user confirmation prompts (booleans)

---
+++ src/defcmd/widgets/confirm.py
@@ -0,0 +1,62 @@
+from defcmd.widgets.base import Widget
+from defcmd.terminal import bold, dim, raw_mode, read_keypress
+
+_UNSET = object()  # Sentinel value to indicate that no default has been set
+
+class ConfirmWidget(Widget):
+    
+    def __init__(
+            self,
+            prompt: str = "Confirm",
+            *,
+            sep: str = "? ",
+            default: bool | None = None,
+    ):
+        self._prompt = prompt
+        self._sep = sep
+        self._default = default
+        self._value = _UNSET
+        self._interacted: bool = False
+
+    def render(self) -> str:
+        label = bold(self._prompt) if self._prompt else ""
+        default_str = self._get_default_str()
+        return f"{label} {default_str}{self._sep}"
+
+    def _get_default_str(self) -> str:
+        res = ""
+        if self._default is True:
+            res = "[Y/n]"
+        elif self._default is False:
+            res = "[y/N]"
+        else:
+            res = "[y/n]"
+        return dim(res)
+
+    @property
+    def value(self) -> bool:
+        if self._value is not _UNSET:
+            return bool(self._value)
+
+        prompt_str = self.render()
+        print(prompt_str, end="", flush=True)
+
+        with raw_mode():
+            while True:
+                key = read_keypress()
+                if key == "enter":
+                    if self._default is not None:
+                        self._value = self._default
+                        print()  # Move to the next line after the prompt
+                        break
+                elif key.lower() == "y":
+                    self._value = True
+                    print('y')  # Move to the next line after the prompt
+                    break
+                elif key.lower() == "n":
+                    self._value = False
+                    print('n')  # Move to the next line after the prompt
+                    break
+                # If the user presses any other key, we simply ignore it and continue prompting for input.
+
+        return self._value
