742b88ab82a49784c49c2da159461ae6006ae149
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-06-29 18:44:14 +0530

    Enhance the UX of the `ConfirmWidget`

--- src/defcmd/widgets/base.py
+++ src/defcmd/widgets/base.py
@@ -11,8 +11,16 @@ class Widget(ABC):
         """Render the widget as a string for display in the terminal"""
         pass
 
+    def prompt(self) -> Any:
+        """Prompt the user for input on a loop and return the value"""
+        pass
+
     @property
     @abstractmethod
     def value(self) -> Any:
-        """Get the current value of the widget"""
+        """
+        Get the current value of the widget.
+        If the widget has not been interacted with yet, this will prompt the user for input.
+        Otherwise, it will return the cached value.
+        """
         pass
--- src/defcmd/widgets/confirm.py
+++ src/defcmd/widgets/confirm.py
@@ -1,11 +1,10 @@
 from defcmd.widgets.base import Widget
-from defcmd.terminal import bold, dim, cyan, raw_mode
+from defcmd.terminal import bold, dim, cyan, green, raw_mode, Cursor
 from defcmd.terminal.reader import InputReader, DefaultInputReader
 
 _UNSET = object()  # Sentinel value to indicate that no default has been set
 
 class ConfirmWidget(Widget):
-    
     def __init__(
             self,
             prompt: str | None = None,
@@ -20,6 +19,7 @@ class ConfirmWidget(Widget):
         self._prompt_suffix = prompt_suffix
         self._default = default
         self._value = _UNSET
+        self._value_str = ""
         self._input_reader = DefaultInputReader() if input_reader is None else input_reader 
         self._interacted: bool = False
 
@@ -38,30 +38,49 @@ class ConfirmWidget(Widget):
             res = "[y/n]"
         return dim(res)
 
+    def render_done(self) -> str:
+        label = bold(self._prompt) if self._prompt else ""
+        checkmark = green("✓")
+        if self._value_str == "":
+            self._value_str = "yes" if self._default is True else "no" if self._default is False else ""
+        return f"{checkmark} {label}{self._prompt_suffix}{self._value_str}"
+
+    def prompt(self) -> bool:
+
+        # Save the current cursor position
+        print(Cursor.save_position(), flush=True, end="")
+
+        # Print the prompt to the terminal
+        print(self.render(), flush=True, end="")
+
+        # Enter raw mode to read single keypresses
+        with raw_mode():
+            while self._value is _UNSET:
+                key = self._input_reader.read_keypress()
+
+                if key == "enter":
+                    if self._default is not None:
+                        self._value = self._default
+                elif key.lower() == "y":
+                    self._value = True
+                    self._value_str = key
+                elif key.lower() == "n":
+                    self._value = False
+                    self._value_str = key
+
+                # If the user presses any other key, we simply ignore it and continue prompting for input.
+
+        # Move the cursor back to the saved position and clear the line
+        print(Cursor.restore_position(), flush=True, end="")
+        print(Cursor.clear_line(), flush=True, end="")
+
+        # Print the final form of the widget
+        print(self.render_done(), flush=True)
+
+        return bool(self._value)
+
     @property
     def value(self) -> bool:
         if self._value is not _UNSET:
             return bool(self._value)
-
-        prompt_str = self.render()
-        print(prompt_str, end="", flush=True)
-
-        with raw_mode():
-            while True:
-                key = self._input_reader.read_keypress()
-                if key == "enter":
-                    if self._default is not None:
-                        self._value = self._default
-                        print()  # Move to the next line after the prompt
-                        break
-                elif key.lower() == "y":
-                    self._value = True
-                    print('y')  # Move to the next line after the prompt
-                    break
-                elif key.lower() == "n":
-                    self._value = False
-                    print('n')  # Move to the next line after the prompt
-                    break
-                # If the user presses any other key, we simply ignore it and continue prompting for input.
-
-        return self._value
+        return self.prompt()
