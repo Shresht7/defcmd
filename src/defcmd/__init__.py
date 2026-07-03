23c069ddd4f7c0e9a59a99dd4aa6f477e8e3dc62
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-07-03 23:39:06 +0530

    Version: `v0.6.0`

--- src/defcmd/__init__.py
+++ src/defcmd/__init__.py
@@ -5,5 +5,5 @@ defcmd provides a simple and intuitive way to turn any function signature into a
 from .runner import cmd, CLI
 from .spec import Spec
 
-__version__ = "0.5.4"
+__version__ = "0.6.0"
 __all__ = ["cmd", "CLI", "Spec"]
